3a64e21cd3e1c595531b24617ef59812c5705aef
Jojo <jojothomas1515@gmail.com>
2023-04-06 16:54:34 +0100

    %S

--- main.c
+++ main.c
@@ -11,11 +11,12 @@ int main(void)
 {
 	int len, len2;
 
-	len = _printf("%X\n", -1024);
-	len2 = printf("%X\n", -1024);
+	len = _printf("%S", "\x01\x02\x03\x04\x05\x06\x07");
+	len2 = printf("\\x01\\x02\\x03\\x04\\x05\\x06\\x07");
 	fflush(stdout);
 	if (len != len2)
 	{
+		printf("%d, %d", len, len2);
 		printf("Lengths differ.\n");
 		fflush(stdout);
 		return (1);
--- utilities.c
+++ utilities.c
@@ -62,3 +62,40 @@ int p_str(char *str)
 
 	return (count);
 }
+
+/**
+ * p_hex - print out the hexadecimal representation of uint
+ * @hex: char in hex
+ * Return: print count
+ */
+int p_hex(int hex)
+{
+	char *h_str = malloc(BUFFER_SIZE);
+	uint num = hex, i;
+	char *arr = "0123456789ABCDEF";
+
+	if (h_str == NULL)
+		return (0);
+
+	for (i = 0; num > 0; i++)
+	{
+		if (num > 15)
+		{
+			h_str[i++] = arr[(num % 16)];
+			h_str[i++] = 'x';
+			h_str[i] = '\\';
+		}
+		else
+		{
+			h_str[i++] = arr[(num % 16)];
+			h_str[i++] = '0';
+			h_str[i++] = 'x';
+			h_str[i] = '\\';
+
+		}
+		num /= 16;
+	}
+	h_str[i] = '\0';
+	_strrev(h_str);
+	return (p_str(h_str));
+}
